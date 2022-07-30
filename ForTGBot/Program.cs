6d38dcdf1e9c4d18bcc88927cc196142a8d21150
akaslavonic <akaslavonic@gmail.com>
2022-07-30 13:05:55 +0400

    OOP task

--- ForTGBot/Program.cs
+++ ForTGBot/Program.cs
@@ -7,19 +7,23 @@ namespace TelegramBotExperiments
 
     class Program
     {
-        static ITelegramBotClient bot = new TelegramBotClient("");
+        public static ColorEnum OurColor = ColorEnum.Red; // Red
+        static ITelegramBotClient bot = new TelegramBotClient("5492114596:AAFxQoIR4y-DJLAub1NqNP1KA-ljJ5t6bJc");
 
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
             CancellationToken cancellationToken)
         {
-            // Некоторые действия
+            // Update 1 01 01 10 10 10 101
+            await botClient.SendTextMessageAsync(update.Message.Chat, "Привет-привет!!");
+            // Некоторые действия 
+            
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
             if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
             {
                 var message = update.Message;
                 if (message.Text.ToLower() == "/start")
                 {
-                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
+                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник хехе!");
                     return;
                 }
 
@@ -33,24 +37,28 @@ namespace TelegramBotExperiments
             // Некоторые действия
             Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
         }
-
-
+        
         static void Main(string[] args)
         {
+            
+            
             Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
 
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
+            
             var receiverOptions = new ReceiverOptions
             {
                 AllowedUpdates = { }, // receive all update types
             };
+
             bot.StartReceiving(
                 HandleUpdateAsync,
                 HandleErrorAsync,
                 receiverOptions,
                 cancellationToken
             );
+            
             Console.ReadLine();
         }
     }
